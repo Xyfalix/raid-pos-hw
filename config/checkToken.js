b3de6d953e1da0371d4a2e0d9887ae78da1555bb
Xyfalix <129175727+Xyfalix@users.noreply.github.com>
2024-01-31 16:01:05 +0800

    feat: set up backend server and connection to mongodb

---
+++ config/checkToken.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+
+const checkToken = async (req, res, next) => {
+    const authHeader = req.get("Authorization");
+    if (!authHeader) {
+        res.status(401).json({ err: "No token in header" });
+        return
+    }
+
+    const authHeaderArray = authHeader.split(" ");
+    const token = authHeaderArray[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+        res.locals.userID = decoded._id;
+        res.locals.userRole = decoded.role;
+        next();
+    } catch (err) {
+        res.status(401).json({ err });
+    }
+};
+
+function checkAdminRole(req, res, next) {
+    if (res.locals.userRole === "admin") {
+        next();
+    } else {
+        res.status(403).json({ err: "Access forbidden"})
+    }
+}
+
+module.exports = {
+    checkToken,
+    checkAdminRole,
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+require("./config/database");
+const express = require("express");
+const path = require("path");
+
+// backend app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+
+// routes
+
+app.get("/api", (req, res) => {
+    res.json({ msg: "Hello World!"})
+})
+
+// listen
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log(`Express app running on port ${port}`)
+});
